e68aed12eb4f66436c231282a48092ad8366b43c
VWmin <fawsuki@gmail.com>
2023-06-27 16:02:13 +0800

    logic of heat matrix

--- heat_degree_matrix.py
+++ heat_degree_matrix.py
@@ -1,9 +1,19 @@
 import networkx as nx
 
-def heatr_degree_matrix(relavence, G:nx.Graph):
+def heat_degree_matrix(relavence, G:nx.Graph, Ex, s, r):
     max_w = 0
     V = G.number_of_nodes()
     for u, v, w in G.edges.data('weight'):
         max_w = max(max_w, w)
-    
+
+    def heat_degree_matrix_ij(Ts, i, j):
+        # 如果边ij是s的候选边，且已在以s为源的现存多播树中
+        # or 边ij是s的候选边，但不在以r为源的现存多播树中，且边ij的带宽满足所有以其为候选边的源的带宽要求之和
+        if s in relavence[i][j]:
+            if G[i][j] in Ts or checkB():
+                return 'case1'
+            else:
+                return 'case2'
+        else :
+            return 'case3'
     
