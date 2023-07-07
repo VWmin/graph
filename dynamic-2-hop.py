7e28ebb09e4374c485cdf8341d5d0f99fd42ce3d
VWmin <fawsuki@gmail.com>
2023-07-07 17:20:35 +0800

    try paralle failed

--- dynamic-2-hop.py
+++ dynamic-2-hop.py
@@ -1,6 +1,10 @@
 import networkx as nx
 import random_graph
-import time
+import numpy as np
+from multiprocessing import Pool
+from multiprocessing.shared_memory import SharedMemory
+from multiprocessing.managers import SharedMemoryManager
+import pickle
 
 inf = 1000000000
 
@@ -39,37 +43,50 @@ def PSL(G:nx.Graph):
         h = h+1
     return L
 
-def WPSL(G:nx.Graph):
-    L = [{v:{v:0} for v in G.nodes}]
 
-    L.append({v:{} for v in G.nodes})
-    h = 1
-
-    def try_get(Lh, u, w):
-        if w not in Lh[u]:
-            return inf
-        return Lh[u][w]
-    
-    def r(u, v):
+def r(G:nx.Graph, u, v):
         if G.degree[u] == G.degree[v]:
             return u < v
         return G.degree[u] < G.degree[v]
 
+def try_get(Lh, u, w):
+        if w not in Lh[u]:
+            return inf
+        return Lh[u][w]
+
+def step(G, L, h, v):
+    cand = set()
+    for u in G.neighbors(v):
+        cand = cand | set(L[h-1][u].keys())
+    for w in cand:
+        if r(G, w, v):
+            continue
+        d = inf
+        for u in G.neighbors(v):
+            d = min(d, G[v][u]['weight']+try_get(L[h-1], u, w))
+        if Q_WPSL(w, v, L, h) <= d:
+            continue
+        hack = L[h]
+        hack[v][w] = d
+        L[h] = hack
+
+def WPSL(G:nx.Graph):
+    L = []
+    L.append({v:{v:0} for v in G.nodes})
+
+    h = 1
     while not empty(G, L[h-1]):
-        for v in G.nodes:
-            cand = set()
-            for u in G.neighbors(v):
-                cand = cand | set(L[h-1][u].keys())
-            for w in cand:
-                if r(w, v):
-                    continue
-                d = inf
-                for u in G.neighbors(v):
-                    d = min(d, G[v][u]['weight']+try_get(L[h-1], u, w))
-                if Q_WPSL(w, v, L, h) <= d:
-                    continue
-                L[h][v][w] = d
         L.append({v:{} for v in G.nodes})
+        
+        # p = Pool()
+        # for v in G.nodes:
+        #     p.apply_async(func=step, args=(shm_G.name, shm_L, G.number_of_nodes, h, v))
+        # p.close()
+        # p.join()
+
+        for v in G.nodes:
+            step(G, L, h, v)
+
         h = h+1
     return L
 
@@ -135,8 +152,8 @@ def test_correctness_psl():
     print("all right")
 
 def test_correctness_wpsl():
-    # G = example_graph()
-    G = random_graph.random_graph(300, 0.1, 100)
+    G = example_graph()
+    # G = random_graph.random_graph(300, 0.1, 100)
     Lwpsl = WPSL(G)
 
     import pll_weighted
@@ -153,13 +170,34 @@ def test_correctness_wpsl():
                 exit(0)
     print("all right")
 
+def test_timecost_wpsl():
+    import time
+    G = random_graph.random_graph(300, 0.1, 100)
+    # G = example_graph()
 
-# test_correctness_psl()
-test_correctness_wpsl()
+    t1 = time.time()
+    Lwpsl = WPSL(G)
+
+    t2 = time.time()
+    print("WPSL time cost: {0}".format(t2-t1))
+
+    import pll_weighted
+    Lwpll = pll_weighted.weighted_pll(G)
+    t3 = time.time()
+    print("WPLL time cost: {0}".format(t3-t2))
+
+def test_profile_wpsl():
+    import line_profiler as lp
+    G = random_graph.random_graph(300, 0.1, 100)
+    profile = lp.LineProfiler(WPSL)
+    profile.add_function(Q_WPSL)
+    profile.enable()
+    WPSL(G)
+    profile.disable()
+    profile.print_stats()
 
 
-
-
-
-
-
+if __name__ == "__main__":
+    # test_correctness_wpsl()
+    test_timecost_wpsl()
+    # test_profile_wpsl()
