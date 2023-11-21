27c2d3c437b7f0cc25d7322249bc65e1c3ec4365
vwmin <fawsuki@gmail.com>
2023-11-21 16:31:57 +0800

    refactor hlmr

--- hlmr.py
+++ hlmr.py
@@ -2,40 +2,79 @@ import itertools
 
 import networkx as nx
 
+import heat_degree_matrix
 import relavence_matrix
 import util
 
 from math import inf
 
 
-def hlmr(g: nx.Graph, s2r, delay_limit, bandwidth_require, heat_function):
-    Ts = {}
-    for s in s2r:
-        Ts[s] = {}
-        for r in s2r[s]:
-            cost, path = hcp(g, s, r, bandwidth_require, heat_function)
-            if cost == inf:
-                return Ts
-            if cost > delay_limit[s]:
-                # 计算s到其他节点的最短路
-                # 计算其他节点到r的最短路
-                rp, minc = None, inf
-                n = len(path)
-                for i in range(n):
-                    for j in range(i + 1, n):
-                        tmp_path = sp(g, path[0], path[i]) + trim(path, i, j) + sp(g, path[j], path[n - 1])
-                        tmp_cost = d(g, tmp_path)
-                        if tmp_cost <= delay_limit[s] and hd(g, tmp_path) < minc:
-                            rp, minc = tmp_path, tmp_cost
-                Ts[s][r] = rp
-            else:
-                Ts[s][r] = path
+class HLMR:
+    def __init__(self, g: nx.Graph, delay_limit, bandwidth_require, s2r):
+        self._g = g
+        self._delay_limit = delay_limit
+        self._bandwidth_require = bandwidth_require
+        self._src2recv = s2r
+        self.routing_trees = {}
+        self._heat = heat_degree_matrix.HeatDegreeBase(self._g, self._delay_limit, self._bandwidth_require,
+                                                       self._src2recv, self.routing_trees, False)
+        self._routing()
 
-            # 更新剩余带宽
-            # for i in range(len(Ts[s][r]) - 1):
-            #     u, v = Ts[s][r][i], Ts[s][r][i+1]
-            #     g[u][v]['bandwidth'] -= bandwidth_require[s]
-    return Ts
+    def _routing(self):
+        for s in self._src2recv:
+            self.routing_trees[s] = {}
+            for r in self._src2recv[s]:
+                cost, path = self.hcp(s, r)
+                if cost == inf:
+                    self.routing_trees[s][r] = []
+                    continue
+                if cost > self._delay_limit[s]:
+                    # 计算s到其他节点的最短路
+                    # 计算其他节点到r的最短路
+                    rp, minc = None, inf
+                    n = len(path)
+                    for i in range(n):
+                        for j in range(i + 1, n):
+                            tmp_path = sp(self._g, path[0], path[i]) + trim(path, i, j) + sp(self._g, path[j],
+                                                                                             path[n - 1])
+                            tmp_cost = d(self._g, tmp_path)
+                            if tmp_cost <= self._delay_limit[s] and hd(self._g, tmp_path) < minc:
+                                rp, minc = tmp_path, tmp_cost
+                    self.routing_trees[s][r] = rp
+                else:
+                    self.routing_trees[s][r] = path
+
+                # 更新剩余带宽
+                for i in range(len(self.routing_trees[s][r]) - 1):
+                    u, v = self.routing_trees[s][r][i], self.routing_trees[s][r][i + 1]
+                    self._g[u][v]['bandwidth'] -= self._bandwidth_require[s]
+
+    def hcp(self, s, r):
+        paths = {s: [s]}
+        dist = {}
+        seen = {s: 0}
+        c = itertools.count()
+        pq = util.PriorityQueue()
+        pq.push((0, next(c), s))
+        while pq.size():
+            _d, _, v = pq.pop()
+            if v in dist:
+                continue
+            dist[v] = _d
+            if v == r:
+                break
+            for u in self._g.neighbors(v):
+                if self._g[v][u]['bandwidth'] < self._bandwidth_require[s]:
+                    continue
+                cost = self._heat.get_heat_degree_ij(s, u, v)
+                vu_dist = dist[v] + cost
+                if u not in seen or vu_dist < seen[u]:
+                    seen[u] = vu_dist
+                    pq.push((vu_dist, next(c), u))
+                    paths[u] = paths[v] + [u]
+        if r not in dist:
+            return inf, None
+        return dist[r], paths[r]
 
 
 def d(g: nx.Graph, path):
@@ -65,38 +104,6 @@ def trim(path, i, j):
     return path[i: j + 1]
 
 
-def hcp(g: nx.Graph, s, r, bandwidth_require, heat_function):
-    # 忽略边：
-    #   1. 现有的以s为根的多播树不包含这条边
-    #   2. 此链路的剩余带宽不满足来自s的多播会话的带宽要求
-    # return nx.single_source_dijkstra(g, s, r)
-    paths = {s: [s]}
-    dist = {}
-    seen = {s: 0}
-    c = itertools.count()
-    pq = util.PriorityQueue()
-    pq.push((0, next(c), s))
-    while pq.size():
-        d, _, v = pq.pop()
-        if v in dist:
-            continue
-        dist[v] = d
-        if v == r:
-            break
-        for u in g.neighbors(v):
-            # if g[v][u]['bandwidth'] < bandwidth_require[s]:
-            #     continue
-            cost = heat_function(s, u, v)
-            vu_dist = dist[v] + cost
-            if u not in seen or vu_dist < seen[u]:
-                seen[u] = vu_dist
-                pq.push((vu_dist, next(c), u))
-                paths[u] = paths[v] + [u]
-    if r not in dist:
-        return inf, None
-    return dist[r], paths[r]
-
-
 def test_hlmr():
     import random_graph
 
@@ -105,8 +112,8 @@ def test_hlmr():
     s2r = {1: {5, 6, 7, 8, 9}}
     delay_limit = {1: 200}
     bandwidth_require = {1: 50}
-    Ts = hlmr(g, s2r, delay_limit, bandwidth_require)
-    print(Ts)
+    instance = HLMR(g, delay_limit, bandwidth_require, s2r)
+    print(instance.routing_trees)
 
 
 if __name__ == '__main__':
