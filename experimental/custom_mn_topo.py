76e63d260340f9cb124baa97a5df48ca1e40cf28
vwmin <fawsuki@gmail.com>
2023-11-27 15:19:46 +0800

    trying to add mininet

---
+++ experimental/custom_mn_topo.py
@@ -0,0 +1,33 @@
+import random_graph
+from mininet.net import Mininet
+
+
+def construct_mininet_from_networkx(graph, host_range):
+    """ Builds the mininet from a networkx graph.
+
+    :param graph: The networkx graph describing the network
+    :param host_range: All switch indices on which to attach a single host as integers
+    :return: net: the constructed 'Mininet' object
+    """
+
+    net = Mininet()
+    # Construct mininet
+    for n in graph.nodes:
+        net.addSwitch("s_%s" % n)
+        # Add single host on designated switches
+        if int(n) in host_range:
+            net.addHost("h%s" % n)
+            # directly add the link between hosts and their gateways
+            net.addLink("s_%s" % n, "h%s" % n)
+    # Connect your switches to each other as defined in networkx graph
+    for (n1, n2) in graph.edges:
+        net.addLink('s_%s' % n1, 's_%s' % n2)
+    return net
+
+
+def ts_example_2_mn_net():
+    g = random_graph.gt_itm_example()
+    return construct_mininet_from_networkx(g, [])
+
+
+topos = {'mytopo': ts_example_2_mn_net()}
