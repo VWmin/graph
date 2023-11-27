ce4e3caf1b4ecc440c2103588daace8338925e87
vwmin <fawsuki@gmail.com>
2023-11-27 16:24:34 +0800

    trying to add mininet

--- experimental/custom_mn_topo.py
+++ experimental/custom_mn_topo.py
@@ -1,9 +1,10 @@
-from mininet.net import Mininet
+from mininet.topo import Topo
 import networkx as nx
 
 
 def gt_itm_example() -> nx.Graph:
-    filename = r"sample-graphs/ts/ts600/ts600-0.alt"
+    # filename = r"sample-graphs/ts/ts600/ts600-0.alt"
+    filename = r"sample-graphs/ts/ts100/ts100-0.alt"
     g = nx.Graph()
     with open(filename, "r") as f:
         flag = False
@@ -30,32 +31,46 @@ def as_733_example():
                 g.add_edge(arr[0], arr[1])
 
 
-def construct_mininet_from_networkx(graph: nx.Graph, host_range):
-    """ Builds the mininet from a networkx graph.
+class MyTopo(Topo):
+    def __init__(self, graph: nx.Graph, host_range):
+        self.graph = graph
+        self.host_range = host_range
+        super().__init__()
 
-    :param graph: The networkx graph describing the network
-    :param host_range: All switch indices on which to attach a single host as integers
-    :return: net: the constructed 'Mininet' object
-    """
-
-    net = Mininet()
-    # Construct mininet
-    for n in graph.nodes:
-        net.addSwitch("s_%s" % n)
-        # Add single host on designated switches
-        if int(n) in host_range:
-            net.addHost("h%s" % n)
+    def build(self):
+        for n in self.graph.nodes:
+            self.addSwitch("s_%s" % n)
+            # Add single host on designated switches
+            # if int(n) in self.host_range:
+            self.addHost("h%s" % n)
             # directly add the link between hosts and their gateways
-            net.addLink("s_%s" % n, "h%s" % n)
-    # Connect your switches to each other as defined in networkx graph
-    for (n1, n2) in graph.edges:
-        net.addLink('s_%s' % n1, 's_%s' % n2)
-    return net
+            self.addLink("s_%s" % n, "h%s" % n)
+        # Connect your switches to each other as defined in networkx graph
+        for (n1, n2) in self.graph.edges:
+            self.addLink('s_%s' % n1, 's_%s' % n2)
 
 
-def ts_example_2_mn_net():
-    g = gt_itm_example()
-    return construct_mininet_from_networkx(g, [])
+# def construct_mininet_from_networkx(graph: nx.Graph, host_range):
+#     """ Builds the mininet from a networkx graph.
+#
+#     :param graph: The networkx graph describing the network
+#     :param host_range: All switch indices on which to attach a single host as integers
+#     :return: net: the constructed 'Mininet' object
+#     """
+#
+#     net = Mininet()
+#     # Construct mininet
+#     for n in graph.nodes:
+#         net.addSwitch("s_%s" % n)
+#         # Add single host on designated switches
+#         if int(n) in host_range:
+#             net.addHost("h%s" % n)
+#             # directly add the link between hosts and their gateways
+#             net.addLink("s_%s" % n, "h%s" % n)
+#     # Connect your switches to each other as defined in networkx graph
+#     for (n1, n2) in graph.edges:
+#         net.addLink('s_%s' % n1, 's_%s' % n2)
+#     return net
 
 
-topos = {'mytopo': ts_example_2_mn_net()}
+topos = {'mytopo': (lambda: MyTopo(gt_itm_example(), []))}
