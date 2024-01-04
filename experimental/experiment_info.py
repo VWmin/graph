e1a86f215d1da2756c76af122857aca22edb7d6c
vwmin <fawsuki@gmail.com>
2024-01-04 15:54:41 +0800

    output exp ev info

--- experimental/experiment_info.py
+++ experimental/experiment_info.py
@@ -1,3 +1,4 @@
+import json
 import random
 
 import networkx as nx
@@ -17,20 +18,37 @@ class ExperimentInfo:
 
         random.seed(42)
 
-        add_attr_with_random_value(self.graph, "bandwidth", b_lo, b_hi)
+        self.total_bw = set_random_bw(self.graph, "bandwidth", b_lo, b_hi)
         add_attr_with_random_value(self.graph, "weight", d_lo, d_hi)
         self.S = random_s_from_graph(self.graph, 5)
         self.S2R = random_s2r_from_graph(self.graph, 5, self.S)
-        self.B = random_d_with_range(self.S, b_req_lo, b_req_hi)
+        self.B = random_b_with_range(self.S, b_req_lo, b_req_hi)
         self.D = random_d_with_range(self.S, d_req_lo, d_req_hi)
 
+        print(f"src set is {self.S}")
         print(f"src to recv is {self.S2R}")
+        print(f"total bw is {self.total_bw}")
+        print(f"bw requirement is {self.B}")
 
         group_no = 1
         self.src_to_group_no = {}
         for s in self.S2R:
             self.src_to_group_no[s] = group_no
             group_no += 1
+
+        output = {
+            "s2r": {},
+            "bw_requirement": self.B,
+            "total_bw": self.total_bw,
+            "src_to_group": self.src_to_group_no
+        }
+        for s in self.S2R:
+            output["s2r"][s] = []
+            for r in self.S2R[s]:
+                output["s2r"][s].append(r)
+
+        with open('ev_setting.json', 'w') as json_file:
+            json.dump(output, json_file, indent=4)
 
     def src_to_group_ip(self, src):
         return f'224.0.1.{self.src_to_group_no[src]}'
@@ -39,6 +57,22 @@ class ExperimentInfo:
 def add_attr_with_random_value(g, name, lo, hi):
     for u, v in g.edges:
         g[u][v][name] = random.randint(lo, hi)
+
+
+def set_random_bw(g, name, lo, hi):
+    tot = 0
+    for u, v in g.edges:
+        bw = random.randint(lo, hi)
+        tot += bw
+        g[u][v][name] = bw
+    return tot
+
+
+def random_b_with_range(S, lo, hi):
+    B = {}
+    for s in S:
+        B[s] = round(random.uniform(lo, hi), 1)
+    return B
 
 
 def random_d_with_range(S, lo, hi):
