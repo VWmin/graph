4f37e99b7d2236f83d1312e6d91dd1575eccfeb2
vwmin <fawsuki@gmail.com>
2024-02-29 21:18:19 +0800

    proxy server between ryu & mn

---
+++ distribution/debugger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+from ryu.cmd import manager
+
+
+def main():
+    sys.argv.append('/home/vwmin/anaconda3/envs/multicast/lib/python3.7/site-packages/ryu/app/simple_switch_13.py')
+    sys.argv.append('--verbose')
+    sys.argv.append('--enable-debugger')
+    sys.argv.append('--observe-links')
+    manager.main()
+
+
+if __name__ == '__main__':
+    main()
